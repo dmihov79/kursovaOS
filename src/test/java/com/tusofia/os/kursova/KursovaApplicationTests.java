dac450c655c5853336ad1af1331c0ff1ae4cae68
dmihov <damyan1mihov@gmail.com>
2022-11-23 12:16:59 +0200

    initial commit

---
+++ src/test/java/com/tusofia/os/kursova/KursovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusofia.os.kursova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
